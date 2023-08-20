802d08b984c51b70b11ad5dc9bf87d6a7d8b8930
Tsuri Yuma <yuma33t@gmail.com>
2023-08-20 20:27:21 +0900

    アップデート情報の通知を表示する機能を追加

--- ShopListApp/ContentView.swift
+++ ShopListApp/ContentView.swift
@@ -1,7 +1,9 @@
 import SwiftUI
+import AppVersionMonitorSwiftUI
 
 struct ContentView: View {
     
+    @State var isAlert: Bool = false
     @State var selection = 1
     
     var body: some View {
@@ -15,6 +17,28 @@ struct ContentView: View {
                 .tabItem() {
                     Label("買い物診断", systemImage: "rotate.3d")
                 }.tag(2)
+        }
+        .alert(isPresented: $isAlert) {
+            Alert(title: Text("お知らせ"), message: Text("最新バージョンがあります"), dismissButton: .default(Text("OK")) {
+                // AppStoreを開く
+                let url = URL(string: "https://apps.apple.com/us/app/%E8%B2%B7%E3%81%84%E7%89%A9%E3%83%AA%E3%82%B9%E3%83%88%E3%82%A2%E3%83%97%E3%83%AA/id6449244738")!
+                // URLを開けるかをチェックする
+                if UIApplication.shared.canOpenURL(url) {
+                    // URLを開く
+                    UIApplication.shared.open(url, options: [:])
+                }
+            })
+        }
+        .appVersionMonitor(id: 6449244738) { status in
+            switch status {
+            case .updateAvailable:
+                isAlert = true
+                print("アップデートがあります")
+            case .updateUnavailable:
+                print("アップデートがありません")
+            case .failure(let error):
+                print("エラーが発生しました: \(error)")
+            }
         }
     }
 }
